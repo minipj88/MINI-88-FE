9854e958e20d24d620633a3ac6a1c3c38cec96bd
Mangrove-Sun <tjsghd0317@gmail.com>
2022-08-26 19:40:16 +0900

    feat: 페이지 라우팅 작성 및 a태그 스타일 초기화 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
-import {useState} from 'react'
-import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
-import AllProducts from './pages/AllProducts'
-import Cart from './pages/Cart'
-import CreditScore from './pages/CreditScore'
-import Home from './pages/Home'
-import OnBoarding from './pages/OnBoarding'
-import Profile from './pages/Profile'
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AllProducts from './pages/AllProducts';
+import CreditScore from './pages/CreditScore';
+import Cart from './pages/Cart';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import OnBoarding from './pages/OnBoarding';
 
 function App() {
-
-  const [userLoginState,setUserLoginState] = useState(false)
   return (
-   <BrowserRouter>
-   <Routes>
-   
-    <Route path="/" element={<Home/>} />
-    <Route path="/allproducts" element={<AllProducts/>} />
-    <Route path="/creditscore" element={<CreditScore/>} />
-    <Route path="/cart" element={<Cart/>} />
-    <Route path="/profile" element={<Profile/>} />
-    <Route path="/signup" element={<Signup/>} />
-    <Route path="/signin" element={<Signin/>} />
-    <Route path="/onboarding" element={<OnBoarding/>} />
-    
-   </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/allproducts" element={<AllProducts/>} />
+        <Route path="/creditscore" element={<CreditScore/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/onboarding" element={<OnBoarding/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
