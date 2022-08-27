a458f0f9b88a448bfd50aa9fdaafa91e09019283
Mangrove-Sun <tjsghd0317@gmail.com>
2022-08-27 13:27:38 +0900

    style: pages내 파일의 파일명과 export 일치하게 수정

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Profile = () => {
+const ProfilePage = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+export default ProfilePage;
