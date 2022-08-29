ecfd6dcb866fd5398fc046412082aed32bc3ea1f
Kim Myungseong <forwarm5891@gmail.com>
2022-08-29 14:13:35 +0900

    feat: 로그인 폼과 athSlice 연결

--- src/components/auth/signin/SigninFooter.tsx
+++ src/components/auth/signin/SigninFooter.tsx
@@ -3,11 +3,12 @@ import AuthFooterLayout from '../../../shared/util/ui/AuthFooterLayout';
 
 interface SigninFooterProps {
   totalValid : boolean;
+  onClick: () => void
 }
 
-const SigninFooter = ({totalValid}:SigninFooterProps) => {
+const SigninFooter = ({totalValid,onClick}:SigninFooterProps) => {
   return (
-    <AuthFooterLayout infoMessage="Eight 계정이 없으신가요?" linkUrl="/signup" linkMessage="클릭하여 계정을 생성하세요." buttonText="로그인" totalValid={totalValid} />
+    <AuthFooterLayout infoMessage="Eight 계정이 없으신가요?" linkUrl="/signup" linkMessage="클릭하여 계정을 생성하세요." buttonText="로그인" totalValid={totalValid} onClick={onClick} />
   );
 };
 
